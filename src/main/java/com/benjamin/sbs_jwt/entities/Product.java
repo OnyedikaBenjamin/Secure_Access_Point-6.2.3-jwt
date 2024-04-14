18566f26cbe5511aac119a5033d2ae4f65ce1a6e
Ben Billion <benbillion62@gmail.com>
2024-04-14 11:57:59 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/entities/Product.java
+++ src/main/java/com/benjamin/sbs_jwt/entities/Product.java
@@ -1,4 +1,14 @@
 package com.benjamin.sbs_jwt.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
 }

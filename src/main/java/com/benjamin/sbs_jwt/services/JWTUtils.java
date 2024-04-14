d1167d8662288b72cf83ecf0e17ec1b30e4d0395
Ben Billion <benbillion62@gmail.com>
2024-04-14 12:21:55 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
+++ src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
@@ -2,6 +2,9 @@ package com.benjamin.sbs_jwt.services;
 
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+
 @Component
 public class JWTUtils {
+    private SecretKey secretKey = ;
 }

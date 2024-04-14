3a8ca83ecf96b50fe0f52b207c06324e8a2773e8
Ben Billion <benbillion62@gmail.com>
2024-04-14 12:34:44 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
+++ src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
@@ -7,4 +7,11 @@ import javax.crypto.SecretKey;
 @Component
 public class JWTUtils {
     private SecretKey secretKey;
+    private static final long EXPIRATION_TIME = 86_400_000;  // 24 hours or 86400000 milli secs
+
+    public JWTUtils (){
+        String secretString = "rh9ih84948484839u4bf84h3i23028202080trv67rb5gw78ibv78w4b9789bwy347vb45yvb847y5v4y574y54y57y457465948394639bfr8vb837438";
+
+    }
+    
 }

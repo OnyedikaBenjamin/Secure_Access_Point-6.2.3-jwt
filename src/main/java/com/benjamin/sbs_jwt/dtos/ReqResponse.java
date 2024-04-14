ea60c787bccd4f0adcc1639ac67f5d4b90982327
Ben Billion <benbillion62@gmail.com>
2024-04-14 07:39:18 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/dtos/ReqResponse.java
+++ src/main/java/com/benjamin/sbs_jwt/dtos/ReqResponse.java
@@ -1,7 +1,9 @@
 package com.benjamin.sbs_jwt.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReqResponse {
 }

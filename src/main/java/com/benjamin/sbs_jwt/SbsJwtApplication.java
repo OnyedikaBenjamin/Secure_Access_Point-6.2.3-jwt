18fa2b6a730cddf6cc6a36b1258a65c7c52f6e0b
Ben Billion <benbillion62@gmail.com>
2024-04-14 07:31:56 +0100

    updated

---
+++ src/main/java/com/benjamin/sbs_jwt/SbsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.sbs_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benjamin/sbs_jwt/SbsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.sbs_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

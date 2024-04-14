23ffea6dbf1f7daf2d0a7adebb195b131c910f4e
Ben Billion <benbillion62@gmail.com>
2024-04-14 07:55:21 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/dtos/RequestResponse.java
+++ src/main/java/com/benjamin/sbs_jwt/dtos/RequestResponse.java
@@ -8,4 +8,13 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true) // This annotation is used during deserialization (converting JSON into Java objects). It instructs Jackson to ignore any properties in the incoming JSON that don't have corresponding fields in the RequestResponse class. This can be helpful when dealing with JSON data that might contain additional properties you don't expect or use in your application. By ignoring them, you avoid potential errors or unexpected behavior.
 @JsonInclude(JsonInclude.Include.NON_NULL)  // This annotation is used during serialization (converting Java objects into JSON). It tells Jackson to only include fields in the resulting JSON that have non-null values. This can help reduce the size of the JSON output and eliminate unnecessary information, especially if your RequestResponse class has many fields.
 public class RequestResponse {
+    private String error;
+    private String error;
+    private String error;
+    private String error;private String error;
+    \\
+    private String error;
+
+
+
 }

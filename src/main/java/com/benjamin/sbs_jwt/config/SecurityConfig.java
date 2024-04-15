1edf7e55b1a4a4ee6fc31040a58a7f9fb0218c68
Ben Billion <benbillion62@gmail.com>
2024-04-15 23:46:20 +0100

    security-config updated

--- src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
+++ src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 );
         return httpSecurity.build();
     }
+
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();  // DaoAuthenticationProvider is explicitly used for loading users from the database or any other data layer and use it for authentication.
@@ -47,6 +48,7 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();

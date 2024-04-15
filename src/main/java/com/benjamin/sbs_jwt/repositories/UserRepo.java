3804fb9e77337154cc20cd4b5ae778d7915f8b6a
Ben Billion <benbillion62@gmail.com>
2024-04-16 00:37:06 +0100

    updated

--- src/main/java/com/benjamin/sbs_jwt/repositories/UserRepo.java
+++ src/main/java/com/benjamin/sbs_jwt/repositories/UserRepo.java
@@ -2,7 +2,6 @@ package com.benjamin.sbs_jwt.repositories;
 
 import com.benjamin.sbs_jwt.entities.OurUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<OurUsers, Integer> {

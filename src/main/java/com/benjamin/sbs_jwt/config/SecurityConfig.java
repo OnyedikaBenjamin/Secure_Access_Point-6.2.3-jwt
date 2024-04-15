96d32307e9ebe9703436cf1c1ad3330fd359adba
Ben Billion <benbillion62@gmail.com>
2024-04-15 23:46:02 +0100

    security-config updated

--- src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
+++ src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();

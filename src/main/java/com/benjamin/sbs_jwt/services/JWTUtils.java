6c6e05015ba81e3b1d1e74c301358b123a706dec
Ben Billion <benbillion62@gmail.com>
2024-04-15 23:59:38 +0100

    refresh token implemented

--- src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
+++ src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
@@ -64,7 +64,6 @@ public class JWTUtils {
         return (username.equals(userDetails.getUsername())
                 && !isTokenExpired(token));
     }
-
     public boolean isTokenExpired(String token){
         return extractClaims(token, Claims::getExpiration)
                 .before(new Date());

3290c8062dabae91276808a9fabc972c7d8a7a5f
Ben Billion <benbillion62@gmail.com>
2024-04-15 04:28:21 +0100

    implemented Auth-Provider

--- src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
+++ src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
@@ -58,4 +58,7 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+    @Bean
+    public Authentication
 }

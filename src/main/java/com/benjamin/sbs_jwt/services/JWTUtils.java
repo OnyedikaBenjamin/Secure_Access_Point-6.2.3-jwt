7277309ed58ab0c0443f8c8adf6e47a56a1add00
Ben Billion <benbillion62@gmail.com>
2024-04-16 00:01:37 +0100

    refresh token implemented

--- src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
+++ src/main/java/com/benjamin/sbs_jwt/services/JWTUtils.java
@@ -58,7 +58,6 @@ public class JWTUtils {
                         .getPayload()
                 );
     }
-
     public boolean isTokenValid(String token, UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())

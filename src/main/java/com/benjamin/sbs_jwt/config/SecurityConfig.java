6dada486a3abb307fec54d77358f8d87c4819315
Ben Billion <benbillion62@gmail.com>
2024-04-15 23:46:29 +0100

    security-config updated

--- src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
+++ src/main/java/com/benjamin/sbs_jwt/config/SecurityConfig.java
@@ -48,7 +48,6 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
